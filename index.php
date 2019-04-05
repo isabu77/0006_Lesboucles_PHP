99983a8dcfa76390d4cdc5128ef79ec62a5f4081
isabu77 <isabu77@gmail.com>
2019-04-05 14:19:14 +0200

    Exercice 2 terminé

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 //LES BOUCLES
 
 // exercice 1
+echo "-- Exercice 1 : " . "<br>";
 $var1 = 0;
 for ($var1 = 0 ; $var1 < 10 ; $var1++){
 	echo $var1 . "<br>";
 }
 
+// exercice 2
+echo "-- Exercice 2 : " . "<br>";
+$x = 0;
+$y = 56;
+for ($x = 0 ; $x <= 20 ; $x++){
+	echo $x*$y . "<br>";
+}
 
 ?>
 <!DOCTYPE html>
