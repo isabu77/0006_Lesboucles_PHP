f320a16903d8c62379827c4254fc6e71718d4a46
isabu77 <isabu77@gmail.com>
2019-04-05 14:25:46 +0200

    Exercice 3 : multiplier de 100 à 10 par 56 -- terminé

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 //LES BOUCLES
 
 // exercice 1
-echo "-- Exercice 1 : " . "<br>";
+echo "-- Exercice 1 : affichage de 0 à 9" . "<br>";
 $var1 = 0;
 for ($var1 = 0 ; $var1 < 10 ; $var1++){
 	echo $var1 . "<br>";
 }
 
 // exercice 2
-echo "-- Exercice 2 : " . "<br>";
+echo "-- Exercice 2 : multiplier de 0 à 20 par 56" . "<br>";
 $x = 0;
 $y = 56;
 for ($x = 0 ; $x <= 20 ; $x++){
 	echo $x*$y . "<br>";
+}
+
+// exercice 3
+echo "-- Exercice 3 : multiplier de 100 à 10 par 56" . "<br>";
+$x = 100;
+$y = 56;
+while (!($x <= 10)){
+	echo $x*$y . "<br>";
+	$x--;
 }
 
 ?>
