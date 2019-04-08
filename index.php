2455558b55e58a07e3354d6703da014f23425680
isabu77 <isabu77@gmail.com>
2019-04-08 11:42:55 +0200

    correction finale

--- index.php
+++ index.php
@@ -2,59 +2,62 @@
 //LES BOUCLES
 
 // exercice 1
-echo "-- Exercice 1 : affichage de 0 à 9" . "<br>";
-$var1 = 0;
-for ($var1 = 0 ; $var1 < 10 ; $var1++){
-	echo $var1 . "<br>";
+echo "-- Exercice 1 : affichage de 0 à 9" . "<br />";
+$x = 0;
+while ($x < 10){
+	echo $x . "<br />";
+	$x++;
 }
 
+
 // exercice 2
-echo "-- Exercice 2 : multiplier de 0 à 20 par 56" . "<br>";
+echo "-- Exercice 2 : multiplier de 0 à 20 par 50" . "<br />";
 $x = 0;
-$y = 56;
-for ($x = 0 ; $x <= 20 ; $x++){
-	echo $x*$y . "<br>";
+$y = 50;
+while ($x <= 20){
+	echo $x*$y . "<br />";
+	$x++;
 }
 
 // exercice 3
-echo "-- Exercice 3 : multiplier de 100 à 10 par 56" . "<br>";
+echo "-- Exercice 3 : multiplier de 100 à 10 par 50" . "<br />";
 $x = 100;
-$y = 56;
-while (!($x <= 10)){
-	echo $x*$y . "<br>";
+$y = 50;
+while ($x > 10){
+	echo $x*$y . "<br />";
 	$x--;
 }
 
 // exercice 4
-echo "-- Exercice 4 : Incrémenter de la moitié de sa valeur de 1 à 10 " . "<br>";
+echo "-- Exercice 4 : Incrémenter de la moitié de sa valeur de 1 à 10 " . "<br />";
 $x = 1;
 while ($x < 10){
-	echo $x . "<br>";
+	echo $x . "<br />";
 	$x += $x/2;
 }
 
 // exercice 5
-echo "-- Exercice 5 : On y arrive presque de 1 à 15 " . "<br>";
+echo "-- Exercice 5 : On y arrive presque de 1 à 15 " . "<br />";
 for ($x = 1 ; $x <= 15 ; $x++){
-	echo "On y arrive presque." . "<br>";
+	echo "On y arrive presque." . "<br />";
 }
 
 // exercice 6
-echo "-- Exercice 6 : On y arrive presque de 20 à 0 " . "<br>";
+echo "-- Exercice 6 : On y arrive presque de 20 à 0 " . "<br />";
 for ($x = 20 ; $x >= 0 ; $x--){
-	echo "C'est presque bon." . "<br>";
+	echo "C'est presque bon." . "<br />";
 }
 
 // exercice 7
-echo "-- Exercice 7 : On tient le bon bout de 1 à 100 par 15 " . "<br>";
+echo "-- Exercice 7 : On tient le bon bout de 1 à 100 par 15 " . "<br />";
 for ($x = 1 ; $x <= 100; $x+=15){
-	echo "On tient le bon bout." . "<br>";
+	echo "On tient le bon bout." . "<br />";
 }
 
 // exercice 8
-echo "-- Exercice 8 : Enfin !!!! de 200 à 0 par 12 " . "<br>";
+echo "-- Exercice 8 : Enfin !!!! de 200 à 0 par 12 " . "<br />";
 for ($x = 200 ; $x >= 0; $x-=12){
-	echo "Enfin !!!!" . "<br>";
+	echo "Enfin !!!!" . "<br />";
 }
 
 
